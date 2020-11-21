77fd45532295c44ad20832779a7007a2a0708957
Alex Malakov <32112772+AlexMHS@users.noreply.github.com>
2020-11-20 21:45:53 -0600

    description
    added todo

--- src/RunGame.java
+++ src/RunGame.java
@@ -1,3 +1,28 @@
+/*
+TODO:
+
+        UI - draw/steal pieces, create working UI
+        -  add a draw square function for the squares
+        -  RunWindow as ui running function
+
+
+        make command line game work - create player's control of game
+        -  implement possibleMoves() function and move function
+        -  include weird pawn move and castling
+        -  piece specific traits:
+        -  pawn changing, king end condition (KING CANNOT BE CHECKED)
+        -  add an apply fog function
+        -  finish player class
+        -  haven't actually figured out what
+
+
+
+
+                networking
+                step 1: figure it out
+                step 2: ???????
+                step 3: working fog of war chess game! */
+
 public class RunGame {
 
     public static void main(String[]args)
